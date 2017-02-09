0b80507055c6a0f17b671eaaa86d42a09a125e7a
Ben Stahl <bens@vimeo.com>
2017-02-09 11:06:12 -0500

    Use the unpacked asar on windows

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-var os = require('os');
 var path = require('path');
 var spawn = require('child_process').spawn;
+
+var PLATFORM = require('os').platform();
+var IS_WINDOWS = /^win[0-9]{2}$/.test(PLATFORM);
 
 var EXECUTABLES = {
     darwin: path.join(__dirname, '..', 'bin/ExManCmd_mac/Contents/MacOS/ExManCmd'),
@@ -22,6 +24,7 @@ var ARGUMENT_HELP = {
     update: 'the extension name.',
 };
 
+
 /**
  * Create a function that invokes the ExManCmd commands. See the docs:
  * https://helpx.adobe.com/extension-manager/using/command-line.html
@@ -32,18 +35,23 @@ var ARGUMENT_HELP = {
 var exManCommand = function(command) {
     return function(argument) {
         return new Promise(function(resolve, reject) {
-            var platform = os.platform();
 
-            if (!(platform in EXECUTABLES)) {
-                return reject('Your current platform is unsupported: ' + platform);
+            if (!(PLATFORM in EXECUTABLES)) {
+                return reject('Your current platform is unsupported: ' + PLATFORM);
             }
 
             if (!argument) {
                 return reject('The "' + command + '" command requires the first argument to be ' + ARGUMENT_HELP[command]);
             }
 
-            var executable = EXECUTABLES[platform];
-            var args = [COMMAND_PREFIXES[platform] + command, argument];
+            var executable = EXECUTABLES[PLATFORM];
+            if (IS_WINDOWS) {
+                // Windows can't navigate the electon asar binary, use the unpacked files
+                executable = executable.replace('app.asar', 'app.asar.unpacked');
+            }
+
+            var args = [COMMAND_PREFIXES[PLATFORM] + command, argument];
+
             var commandProcess = spawn(executable, args);
 
             commandProcess.stdout.on('data', reject);
